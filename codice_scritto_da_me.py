25564ba80a813e7c0d1ba41f0ab0e0a989a8a181
Gekgronda <giacomogronda0105@gmail.com>
2024-10-10 15:33:46 +0200

    rem pandas

--- codice_scritto_da_me.py
+++ codice_scritto_da_me.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
+
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
